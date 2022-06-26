6798f098c9debd6a62388ef15f6cfead8b8c4192
KonulHuseynli <konulrh@code.edu.az>
2022-06-26 20:52:37 +0400

    second part

---
+++ Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp88
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public int BirthDate { get; set; }
+        public double Salary { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string RoleType { get; set; }
+        private int _id;
+
+        public Employee(string username="konulh03",string name = "konul", string surname = "huseynli", string password = "K03#hus2@", string roloetype ="Admin")
+        {
+            _id++;
+            Id = _id;
+            UserName = username;
+            Name = name;
+            SurName = surname;
+            Password = password;
+            RoleType = roloetype;
+           
+        }
+        
+    }
+}
---
+++ Pharmacy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp88
+{
+    class Pharmacy
+    {
+        List<Employee> employerss = new List<Employee>();
+        List<Drug> drugss = new List<Drug>();
+
+       // private List<Employee> _employers;
+       // private List<Drug> _drugs;
+        public int Id { get; set; }
+        public double MinSalary { get; set; }
+        public double Budget { get; set; }
+        public string Location { get; set; }
+        public string Name { get; set; }
+        private int _id;
+
+        public Pharmacy(string name = "BioNTech", double minsalary = 750, double budget = 9.000500, string location = "Germany")
+        {
+            _id++;
+            Id = _id;
+            Name = name;
+            MinSalary = minsalary;
+            Budget = budget;
+            Location = location;
+        }
+        
+
+    }
+}
