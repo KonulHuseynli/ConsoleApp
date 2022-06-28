119e2f5a132d582cc762e8652582ab1591952d1e
KonulHuseynli <konulrh@code.edu.az>
2022-06-28 23:30:45 +0400

    added end task

---
+++ Drug.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp88
+{
+    class Drug
+    {
+        public int Id { get; set; }
+        public int Count { get; set; }
+        public double PurchasePrice { get; set; }
+        public double SalePrice { get; set; }
+        public string Name { get; set; }
+        public string DrugType { get; set; }
+        private int _id;
+        public Drug()
+        {
+            _id++;
+            Id = _id;
+        }
+
+    }
+}
